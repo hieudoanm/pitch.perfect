e08e28a79509a4346ab519bd5a4c570c8387d620
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 22:48:18 +0700

    update cap

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.github.hieudoanm.pitch.perfect',
-  appName: '@pitch.perfect/app',
+  appName: 'Pitch Perfect',
   webDir: '../../docs',
   android: { path: 'mobile/android' },
   ios: { path: 'mobile/ios' },
